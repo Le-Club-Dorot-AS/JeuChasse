b4d80426a400f753f286e4e6638a6e382123dae5
AlexandreBuzon <77610175+AlexandreBuzon@users.noreply.github.com>
2021-01-18 21:58:03 +0100

    barre de vie

---
+++ Assets/Health.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+    public int nbHeart;
+    public Image[] hearts;
+    public Sprite fullHeart;
+    // Update is called once per frame
+    void Update()
+    {
+        nbHeart = health;
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < nbHeart)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+}
