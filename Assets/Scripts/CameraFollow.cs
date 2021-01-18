2b8dce061c7baa6be88465844198efd477076c3b
AlexandreBuzon <77610175+AlexandreBuzon@users.noreply.github.com>
2021-01-18 19:06:17 +0100

    cameraFollow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    public float timeOffset;
+    public Vector3 possOffset;
+
+    private Vector3 velocity;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + possOffset, ref velocity, timeOffset);
+    }
+}
