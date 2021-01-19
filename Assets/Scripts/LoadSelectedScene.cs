288783d9c4e1d1a735799a3fa781cd15d7f4b376
Coconutzsz <75502445+Coconutzsz@users.noreply.github.com>
2021-01-19 22:23:32 +0100

    LevelUpdate

--- Assets/Scripts/LoadSelectedScene.cs
+++ Assets/Scripts/LoadSelectedScene.cs
@@ -3,12 +3,23 @@ using UnityEngine;
 
 public class LoadSelectedScene : MonoBehaviour
 {
+<<<<<<< Updated upstream
     public string SceneName;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
             SceneManager.LoadScene(SceneName);
+=======
+    public string sceneName;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneName);
+>>>>>>> Stashed changes
         }
     }
+
 }
