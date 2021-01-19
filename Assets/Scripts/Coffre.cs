aabb59bfea77df668a69cca971b0deb1c482726f
AlexandreBuzon <77610175+AlexandreBuzon@users.noreply.github.com>
2021-01-19 17:06:17 +0100

    coffre

--- Assets/Scripts/Coffre.cs
+++ Assets/Scripts/Coffre.cs
@@ -7,27 +7,46 @@ public class Coffre : MonoBehaviour
 {
     private bool isInRange; //est-il à porté d'interaction avec l'objet
     public Text interactUI;
+
     public bool etat; //ouvert ou fermer
-    public int contenu;//1 = intru1, 2 = instru2, 3 = intru3, 4 = pieces
-    public int nbPieces;
+    public int contenu;//1 = intru1, 2 = instru2, 3 = intru3, 4 = pieces, 5= papier
+    private int nbPieces;
+    public Text indice;
 
     public Animator animator;
-    // Update is called once per frame
+    private void Awake()
+    {
+        interactUI = GameObject.FindGameObjectWithTag("InteractUI").GetComponent<Text>();
+        interactUI.enabled = false;
+    }
     void Update()
     {
         if (isInRange && Input.GetKeyDown(KeyCode.E) && !etat) //ouvre coffre
         {
-            if (contenu == 1 || contenu == 2 || contenu == 3) {
-                Inventory.instance.AddInstru(contenu);
-            }
-            else
-            {
-                nbPieces = Random.Range(5, 21);
-                Inventory.instance.AddCoins(nbPieces);
-            }
-            etat = true;
-            animator.SetBool("etat", etat);
+            OpenChest();
         }
+    }
+    void OpenChest()
+    {
+        animator.SetTrigger("OpenChest");
+        if (contenu == 1 || contenu == 2 || contenu == 3)
+        {
+            Inventory.instance.AddInstru(contenu);
+            //son instru 
+        }else if(contenu == 5)
+        {
+            Inventory.instance.AddPapier(indice.text);
+            //son indice
+        }
+        else
+        {
+            nbPieces = Random.Range(5, 21);
+            Inventory.instance.AddCoins(nbPieces);
+            //son piece
+        }
+        etat = true;
+        animator.SetBool("state", etat);
+        GetComponent<BoxCollider2D>().enabled = false;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
