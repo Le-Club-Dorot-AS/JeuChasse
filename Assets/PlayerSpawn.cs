00c8098505f84ea2eaa26210890512054876e242
Héloïse/Ezrania <heloise.lamaury@etu.umontpellier.fr>
2021-01-19 13:53:15 +0100

    Level update

---
+++ Assets/PlayerSpawn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GameObject.FindGameObjectWithTag("Player").transform.position = transform.position;
+    }
+
+    
+}
---
+++ Assets/WeakSpot.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public class WeakSpot : MonoBehaviour
+{
+
+    public GameObject Detruire;
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(Detruire);
+        }
+    }
+
+
+
+}
