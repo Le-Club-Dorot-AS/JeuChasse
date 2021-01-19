c20abbf79c66ab79c18412f8e9e7980eb69d9575
Sophie <sophiefoucrier@hotmail.fr>
2021-01-20 00:17:24 +0100

    MainMenu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string LevelToLoad;
+
+    public GameObject settingswindow;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(LevelToLoad);
+    }
+    public void SettingsButton()
+    {
+        settingswindow.SetActive(true);
+    }
+    public void CloseSettingsWindow()
+    {
+        settingswindow.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+}
+
