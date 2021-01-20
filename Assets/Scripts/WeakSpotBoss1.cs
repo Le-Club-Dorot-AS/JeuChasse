86f6f780f6fdaf38da72c4ecfd39a810bdac3a20
Coconutzsz <75502445+Coconutzsz@users.noreply.github.com>
2021-01-20 08:42:40 +0100

    Update

---
+++ Assets/Scripts/WeakSpotBoss1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeakSpotBoss1 : MonoBehaviour
+{
+    public GameObject NomBoss;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            NomBoss.currentHealth = GameObject.parent.currentHealth --;
+        }
+    }
+}
